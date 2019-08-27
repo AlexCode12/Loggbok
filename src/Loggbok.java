92cc383a2a74a7fa7f6492e47b9fe93d68344aad
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2019-08-27 10:30:08 +0200

    Menyn skapades.

---
+++ src/Loggbok.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Loggbok {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Välkommen till loggboken. Välj ett av alternativen nedan genom att skriva in dess siffra.");
+        System.out.println("1. Visa loggboken." + "\n"
+                + "2. Lägg till ett inlägg." + "\n"
+                + "3. Uppdatera." + "\n"
+                + "4. Spara." + "\n"
+                + "5. Läs in." + "\n"
+                + "6. Avsluta.");
+
+    }
+}
+
+
+/*
+Programmet börjar med att menyn visas. I denna finns sex alternativ. 
+ */
