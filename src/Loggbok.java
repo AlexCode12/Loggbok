77f52bf61dd0720984f74ac6c31403c2b4869503
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2019-08-27 11:06:31 +0200

    Grunden på menyn.

--- src/Loggbok.java
+++ src/Loggbok.java
@@ -10,11 +10,28 @@ public class Loggbok {
                 + "4. Spara." + "\n"
                 + "5. Läs in." + "\n"
                 + "6. Avsluta.");
+        int input = sn.nextInt();
+        if (input == 1) {
+            System.out.println("Här kommer loggboken att finnas.");
+        } else if (input == 2) {
+            System.out.println("Det går inte att skriva inlägg just nu.");
+        } else if (input == 3) {
 
+        } else if (input == 4) {
+
+        } else if (input == 5) {
+
+        } else if (input == 6) {
+            System.out.println("Tack, hejdå.");
+            System.exit(0);
+        } else {
+            System.out.println("Detta är inget korrekt tecken.");
+        }
     }
 }
 
 
 /*
-Programmet börjar med att menyn visas. I denna finns sex alternativ. 
+Programmet börjar med att menyn visas. I denna finns sex alternativ. Tänk dig som en telefonsvarare. "För att spara en fil, tryck 4" t.ex, lite åt det hållet tänker jag med menyn.
+Man börjar med att skriva in ett nummer från 1-6, precis som menyn säger. Sedan blir det naturligtvis olika beroende på vilket nummer som väljs.
  */
