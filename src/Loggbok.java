3d4d87db35d4d345e21b7144d65a4f7d54998307
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2019-09-03 08:45:08 +0200

    Allt placeras i metoder.

--- src/Loggbok.java
+++ src/Loggbok.java
@@ -3,13 +3,11 @@ import java.util.Scanner;
 public class Loggbok {
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
-        System.out.println("Välkommen till loggboken. Välj ett av alternativen nedan genom att skriva in dess siffra.");
-        System.out.println("1. Visa loggboken." + "\n"
-                + "2. Lägg till ett inlägg." + "\n"
-                + "3. Uppdatera." + "\n"
-                + "4. Spara." + "\n"
-                + "5. Läs in." + "\n"
-                + "6. Avsluta.");
+        printMenu();
+        selectOptions(sn);
+    }
+
+    private static void selectOptions(Scanner sn) {
         int input = sn.nextInt();
         if (input == 1) {
             System.out.println("Här kommer loggboken att finnas.");
@@ -27,6 +25,16 @@ public class Loggbok {
         } else {
             System.out.println("Detta är inget korrekt tecken.");
         }
+    }
+
+    private static void printMenu() {
+        System.out.println("Välkommen till loggboken. Välj ett av alternativen nedan genom att skriva in dess siffra.");
+        System.out.println("1. Visa loggboken." + "\n"
+                + "2. Lägg till ett inlägg." + "\n"
+                + "3. Uppdatera." + "\n"
+                + "4. Spara." + "\n"
+                + "5. Läs in." + "\n"
+                + "6. Avsluta.");
     }
 }
 
